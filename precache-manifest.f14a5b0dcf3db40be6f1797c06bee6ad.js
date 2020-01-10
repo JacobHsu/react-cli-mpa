7a89b8523bd0205cf40830627885f904cc5dd861
jacobhsu <jacob.hsu.tw@gmail.com>
2020-01-10 16:55:10 +0800

    Updates

---
+++ precache-manifest.f14a5b0dcf3db40be6f1797c06bee6ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3736fbbf14d5204bdc1e28ce7271d7f9",
+    "url": "/react-cli-mpa/index.html"
+  },
+  {
+    "revision": "a954242129148ff3db4c701ea0973696",
+    "url": "/react-cli-mpa/index2.html"
+  },
+  {
+    "revision": "0ab22b5893430a225819",
+    "url": "/react-cli-mpa/static/css/index.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c6c8ea0ff1f41fa817ce",
+    "url": "/react-cli-mpa/static/js/0.2dc906e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-cli-mpa/static/js/0.2dc906e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ab22b5893430a225819",
+    "url": "/react-cli-mpa/static/js/index.94acc3f2.chunk.js"
+  },
+  {
+    "revision": "668c9a998912d2eef146",
+    "url": "/react-cli-mpa/static/js/index2.45d19216.chunk.js"
+  },
+  {
+    "revision": "df9982c09d244d38062e",
+    "url": "/react-cli-mpa/static/js/runtime-index.57fe2465.js"
+  },
+  {
+    "revision": "e1e24395529b347a90b3",
+    "url": "/react-cli-mpa/static/js/runtime-index2.4314a53d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cli-mpa/static/media/logo.5d5d9eef.svg"
+  }
+]);
